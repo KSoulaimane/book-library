837306a91f364a34b7b7155109f0e4eaaa9965c1
Soulaimane Kassim <soulaimanekassim@MacBook-Air-de-Soulaimane.local>
2026-03-06 09:10:42 +0100

    refactor

--- src/app/components/book-form/book-form.component.ts
+++ src/app/components/book-form/book-form.component.ts
@@ -5,13 +5,13 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { Book } from '../../models/book';
-
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-book-form',
   standalone: true,
   imports: [
     CommonModule, 
-    FormsModule, 
+    ReactiveFormsModule, 
     MatFormFieldModule, 
     MatInputModule, 
     MatButtonModule,
@@ -21,30 +21,31 @@ import { Book } from '../../models/book';
 })
 export class BookFormComponent {
   @Output() addBook = new EventEmitter<Book>();
-
-  titre = '';
-  auteur = '';
-  categorie = '';
-  annee!: number;
-  disponible = true;
-
+  bookForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.bookForm = this.fb.group({
+      titre: ['', Validators.required],
+      auteur: [''],
+      categorie: [''],
+      annee: [''],
+      disponible: [true]
+    });
+  }
   submitForm() {
-    if (!this.titre) return; // empêche l'ajout si le titre est vide
-    const newBook: Book = {
-      id: Date.now(), // id simple unique
-      titre: this.titre,
-      auteur: this.auteur,
-      categorie: this.categorie,
-      annee: this.annee,
-      disponible: this.disponible
-    };
-    this.addBook.emit(newBook);
 
-    // Réinitialiser le formulaire
-    this.titre = '';
-    this.auteur = '';
-    this.categorie = '';
-    this.annee = undefined!;
-    this.disponible = true;
+    if (this.bookForm.invalid) {
+      return;
+    }
+  
+    const newBook: Book = {
+      id: Date.now(),
+      ...this.bookForm.value
+    };
+  
+    this.addBook.emit(newBook);
+  
+    this.bookForm.reset({
+      disponible: true
+    });
   }
 }
